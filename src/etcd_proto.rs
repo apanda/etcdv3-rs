c9bc1022463a17b43c0a871fee3667a72ffbf3dc
Aurojit Panda <apanda@cs.berkeley.edu>
2017-07-13 17:40:57 -0700

    Initial commit
    - This has json parsing for Put and Range queries.
    - Some basic testing when connected.

---
+++ src/etcd_proto.rs
@@ -0,0 +1,146 @@
+use base64;
+/// Hand crafted structures from the `etcd` protobuf. This allows us to call into etcd from Rust.
+/// Some weird things to note when adding to this file:
+///   - All fields in proto3 are optional -- as a result every field needs to be marked as an `Option<T>`. This is
+///   necessary to guarantee that changes in the `etcd` implementation do not cause problems.
+///   - The `gRPC` bridge converse 64-bit numbers into Strings. 
+
+/// Common response header included in every `etcd` response.
+#[derive(Deserialize)]
+pub struct ResponseHeader {
+    pub cluster_id: Option<String>, // `Option<String>` instead of `u64` since this is what grpc does.
+    pub member_id: Option<String>,
+    pub revision: Option<String>,
+    pub raft_term: Option<String>,
+}
+
+/// Mechanism to encode `etcd` key-value responses.
+#[derive(Deserialize)]
+pub struct KeyValue {
+    key: Option<String>,
+    pub create_revision: Option<String>,
+    pub mod_revision: Option<String>,
+    pub version: Option<String>,
+    value: Option<String>,
+    pub lease: Option<String>,
+}
+
+impl KeyValue {
+    pub fn key(&self) -> Option<String> {
+        match self.key {
+            Some(ref k) => base64::decode(&k).ok().map(|v| String::from_utf8(v).unwrap()),
+            None => None
+        }
+    }
+
+    pub fn value(&self) -> Option<String> {
+        match self.value {
+            Some(ref v) => base64::decode(&v).ok().map(|v| String::from_utf8(v).unwrap()),
+            None => None
+        }
+    }
+}
+
+/// Response for a `PutRequest`.
+#[derive(Deserialize)]
+pub struct PutResponse {
+    pub header: Option<ResponseHeader>,
+    pub prev_kv: Option<KeyValue>, // Optional since is only set when prev_kv is true.
+}
+
+/// A `PutRequest` to add or overwrite a key for etcd.
+#[derive(Serialize)]
+pub struct PutRequest {
+    pub key: Option<String>,
+    pub value: Option<String>,
+    pub lease: Option<String>,
+    pub prev_kv: Option<bool>,
+    pub ignore_value: Option<bool>,
+    pub ignore_lease: Option<bool>,
+}
+
+impl PutRequest {
+    /// Create a simple `PutRequest`.
+    pub fn new(key: &str, val: &str) -> PutRequest {
+        PutRequest {
+            key: Some(base64::encode(key)),
+            value: Some(base64::encode(val)),
+            lease: None,
+            prev_kv: None,
+            ignore_value: None,
+            ignore_lease: None,
+        }
+    }
+
+    pub fn new_with_previous(key: &str, val: &str) -> PutRequest {
+        PutRequest {
+            prev_kv: Some(true), // Do not get previous by default
+            ..PutRequest::new(key, val)
+        }
+    }
+}
+
+// This is by default turned into a string by `serde_json`, hence encoding it correctly.
+#[derive(Serialize)]
+pub enum SortOrder {
+    NONE,
+    ASCEND,
+    DESCEND
+}
+
+// This is by default turned into a string by `serde_json`, hence encoding it correctly.
+#[derive(Serialize)]
+pub enum SortTarget {
+    KEY,
+    CREATE,
+    VERSION,
+    MOD,
+    VALUE
+}
+
+#[derive(Serialize, Default)]
+pub struct RangeRequest {
+    pub key: Option<String>,
+    pub range_end: Option<String>,
+    pub limit: Option<String>,
+    pub revision: Option<String>,
+    pub sort_order: Option<SortOrder>,
+    pub sort_target: Option<SortTarget>,
+    pub serializable: Option<bool>,
+    pub keys_only: Option<bool>,
+    pub count_only: Option<bool>,
+    pub min_mod_revision: Option<String>,
+    pub max_mod_revision: Option<String>,
+    pub min_create_revision: Option<String>,
+    pub max_create_revision: Option<String>,
+}
+
+impl RangeRequest {
+    pub fn new(key: &str) -> RangeRequest {
+        RangeRequest {
+            key: Some(base64::encode(key)),
+            ..Default::default()
+        }
+    }
+    pub fn new_with_sort(key: &str, order: SortOrder, target: SortTarget) -> RangeRequest {
+        RangeRequest {
+            sort_order: Some(order),
+            sort_target: Some(target),
+            ..RangeRequest::new(key)
+        }
+    }
+}
+
+#[derive(Deserialize)]
+pub struct RangeResponse {
+    pub header: Option<ResponseHeader>,
+    pub kvs: Option<Vec<KeyValue>>, // Optional since is only set when prev_kv is true.
+    pub more: Option<bool>,
+    count: Option<String>
+}
+
+impl RangeResponse {
+    pub fn count(&self) -> usize {
+        self.count.as_ref().map(|v| v.parse::<usize>().unwrap()).unwrap_or(0)
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,93 @@
+#![deny(warnings)]
+extern crate serde;
+extern crate serde_json;
+extern crate base64;
+extern crate futures;
+extern crate hyper;
+extern crate tokio_core;
+
+#[macro_use]
+extern crate serde_derive;
+mod etcd_proto;
+
+pub use self::etcd_proto::*;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json;
+    use hyper;
+    use tokio_core;
+    use futures::Future;
+    use futures::stream::Stream;
+    #[test]
+    fn basic_test() {
+        let req = super::PutRequest::new("hello", "world 22");
+        let json = serde_json::to_string(&req).unwrap();
+        println!("{}", json);
+        let json = serde_json::to_string(&super::PutRequest::new_with_previous("hello", "world 23")).unwrap();
+        println!("{}", json);
+        let resp_json = r#"{"header":{"cluster_id":"14841639068965178418",
+                                      "member_id":"10276657743932975437",
+                                      "revision":"2",
+                                      "raft_term":"4"}}"#;
+        let parsed:PutResponse = serde_json::from_str(resp_json).unwrap();
+        println!("{}", parsed.header.unwrap().cluster_id.unwrap());
+        let resp_json=r#"{"header":{"cluster_id":"14841639068965178418",
+                                    "member_id":"10276657743932975437",
+                                    "revision":"6",
+                                    "raft_term":"4"},
+                          "prev_kv":{"key":"aGVsbG8=",
+                                     "create_revision":"2",
+                                     "mod_revision":"5",
+                                     "version":"4",
+                                     "value":"d29ybGQgMjI="}}"#;
+        let parsed:PutResponse = serde_json::from_str(resp_json).unwrap();
+        let prev_kv = parsed.prev_kv.unwrap();
+        println!("{} --> {}", prev_kv.key().unwrap(), prev_kv.value().unwrap());
+        let json_range = serde_json::to_string(&RangeRequest::new_with_sort("hello", 
+                                               SortOrder::ASCEND, SortTarget::VALUE)).unwrap();
+        println!("{}", json_range);
+    }
+
+    #[test]
+    fn connected_test() {
+        let put_uri = "http://localhost:2379/v3alpha/kv/put".parse::<hyper::Uri>().unwrap();
+        let range_uri = "http://localhost:2379/v3alpha/kv/range".parse::<hyper::Uri>().unwrap();
+        let mut core = tokio_core::reactor::Core::new().unwrap();
+        let handle = core.handle();
+        let client = hyper::Client::new(&handle);
+        let mut put_request = hyper::Request::new(hyper::Method::Post, put_uri);
+        put_request.set_body(serde_json::to_string(&super::PutRequest::new("hello", "world 3333")).unwrap());
+        let work = client.request(put_request).and_then(|res| {
+            println!("Response: {}", res.status());
+            res.body().concat2().and_then(|body: hyper::Chunk| {
+                let v: PutResponse = serde_json::from_slice(&body).unwrap();
+                println!("Cluster ID for put is {}", v.header.unwrap().cluster_id.unwrap());
+                Ok(())
+            })
+        }).and_then(|_| {
+            let mut range_request = hyper::Request::new(hyper::Method::Post, range_uri);
+            range_request.set_body(serde_json::to_string(&super::RangeRequest::new("hello")).unwrap());
+            client.request(range_request)
+        }).and_then(|res| {
+            println!("Response: {}", res.status());
+            res.body().concat2().and_then(|body: hyper::Chunk| {
+                let v: RangeResponse = serde_json::from_slice(&body).unwrap();
+                println!("Returned {} value(s)", v.count());
+                assert!(v.count() == 0 || v.kvs.as_ref().unwrap().len() == v.count());
+                assert!(v.count() == 1);
+                for kv in v.kvs.as_ref().unwrap() {
+                    println!(r#""{}" --> "{}""#, kv.key().unwrap(), kv.value().unwrap());
+                }
+                Ok(v.kvs.unwrap())
+            })
+        }).and_then(|kvs| {  
+            for kv in &kvs {
+                println!(r#""{}" --> "{}""#, kv.key().unwrap(), kv.value().unwrap());
+            };
+            Ok(())
+        });
+        core.run(work).unwrap();
+    }
+}
